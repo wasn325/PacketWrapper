a4659be26161d9924ec91db58a3964c04c1eb960
Max Nestorovich <Rocknest@users.noreply.github.com>
2015-02-14 02:23:23 +0200

    Add missing methods for BlockChange

--- PacketWrapper/src/main/java/com/comphenix/packetwrapper/WrapperPlayServerBlockChange.java
+++ PacketWrapper/src/main/java/com/comphenix/packetwrapper/WrapperPlayServerBlockChange.java
@@ -3,6 +3,7 @@ package com.comphenix.packetwrapper;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.wrappers.BlockPosition;
+import com.comphenix.protocol.wrappers.WrappedBlockData;
 
 public class WrapperPlayServerBlockChange extends AbstractPacket {
     public static final PacketType TYPE = PacketType.Play.Server.BLOCK_CHANGE;
@@ -34,6 +35,19 @@ public class WrapperPlayServerBlockChange extends AbstractPacket {
         handle.getBlockPositionModifier().write(0, value);
     }
     
-    // Cannot find type for block
+    /**
+     * Retrieve Block Data.
+     * @return The current Block Data
+     */
+    public WrappedBlockData getBlockData() {
+    	return handle.getBlockData().read(0);
+    }
+    
+    /**
+     * Set Block Data.
+     * @param value - new value.
+     */
+    public void setBlockData(WrappedBlockData value) {
+    	handle.getBlockData().write(0, value);
+    }
 }
-
