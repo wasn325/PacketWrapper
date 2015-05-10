6f70df90dbbc85935dac1dc68b6ddfac85055ce4
Dan Mulloy <dmulloy2@live.com>
2015-05-10 14:09:42 -0400

    Update MultiBlockChange wrapper to utilize the ProtocolLib wrapper

--- PacketWrapper/src/main/java/com/comphenix/packetwrapper/WrapperPlayServerMultiBlockChange.java
+++ PacketWrapper/src/main/java/com/comphenix/packetwrapper/WrapperPlayServerMultiBlockChange.java
@@ -21,6 +21,7 @@ package com.comphenix.packetwrapper;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.wrappers.ChunkCoordIntPair;
+import com.comphenix.protocol.wrappers.MultiBlockChangeInfo;
 
 public class WrapperPlayServerMultiBlockChange extends AbstractPacket {
     public static final PacketType TYPE = PacketType.Play.Server.MULTI_BLOCK_CHANGE;
@@ -33,44 +34,36 @@ public class WrapperPlayServerMultiBlockChange extends AbstractPacket {
     public WrapperPlayServerMultiBlockChange(PacketContainer packet) {
         super(packet, TYPE);
     }
-    
-    /**
-     * Retrieve Chunk X.
-     * <p>
-     * Notes: chunk X coordinate
-     * @return The current Chunk X
-     */
-    public ChunkCoordIntPair getChunkX() {
-        return handle.getChunkCoordIntPairs().read(0);
-    }
-    
-    /**
-     * Set Chunk X.
-     * @param value - new value.
-     */
-    public void setChunkX(ChunkCoordIntPair value) {
-        handle.getChunkCoordIntPairs().write(0, value);
-    }
-    
-    /**
-     * Retrieve Chunk Z.
-     * <p>
-     * Notes: chunk Z Coordinate
-     * @return The current Chunk Z
-     */
-    public ChunkCoordIntPair getChunkZ() {
-        return handle.getChunkCoordIntPairs().read(0);
-    }
-    
-    /**
-     * Set Chunk Z.
-     * @param value - new value.
-     */
-    public void setChunkZ(ChunkCoordIntPair value) {
-        handle.getChunkCoordIntPairs().write(0, value);
-    }
-    
-    // Cannot find type for b
-    // Cannot find type for b
-}
 
+    /**
+     * Retrieve the chunk that has been altered.
+     * @return The current chunk
+     */
+    public ChunkCoordIntPair getChunk() {
+        return handle.getChunkCoordIntPairs().read(0);
+    }
+
+    /**
+     * Set the chunk that has been altered.
+     * @param value - new value
+     */
+    public void setChunk(ChunkCoordIntPair value) {
+        handle.getChunkCoordIntPairs().write(0, value);
+    }
+
+    /**
+     * Retrieve a copy of the record data as a block change array.
+     * @return The copied block change array.
+     */
+    public MultiBlockChangeInfo[] getRecords() {
+        return handle.getMultiBlockChangeInfoArrays().read(0);
+    }
+
+    /**
+     * Set the record data using the given helper array.
+     * @param value - new value
+     */
+    public void setRecords(MultiBlockChangeInfo[] value) {
+        handle.getMultiBlockChangeInfoArrays().write(0, value);
+    }
+}
